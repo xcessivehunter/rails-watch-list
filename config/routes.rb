857e991ffc03e66e0fcb03ebc5cad4c8a65922fe
Hugo Pereira <hugop1997@gmail.com>
2023-08-18 11:34:47 +0100

    added platform to bundle lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'lists#index'
+  resources :lists, only: %i[index new show create destroy] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies, only: %i[index show new create edit update destroy]
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+Movie.destroy_all
+List.destroy_all
+
+# Create some lists
+names = %w[90s GOAT Netflix SciFi Documentary Horror Action Romance Comdey]
+
+names.each do |name|
+  List.create!(name: name)
+end
+
+puts "Created #{List.count} movie lists 📝"
+
+
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+response['results'].each do |movie_hash|
+  puts
+  p movie_hash
+  # create an instance with the hash
+  Movie.create!(
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+    rating: movie_hash['vote_average'],
+    title: movie_hash['title'],
+    overview: movie_hash['overview']
+  )
+end
+
+puts "Created #{Movie.count} movies 📽️"
